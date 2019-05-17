32c00ab1cac35ecb74e51fc24ec047a8caa7bada
benjagarc <benjamingarcia842@gmail.com>
2019-05-17 10:27:57 -0500

    primer commit

---
+++ build/precache-manifest.d16bd23f8a2c322630edd6ee866523ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7fd15c0669c2baa781c88c7f767615",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70545e4f05bd965f47f7",
+    "url": "/static/css/main.a6a091b5.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "70545e4f05bd965f47f7",
+    "url": "/static/js/main.64534b20.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
